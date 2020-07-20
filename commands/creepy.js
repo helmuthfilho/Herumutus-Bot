189325c5aa2f18129e57ba78af7722869fe79c0e
Ashwurz <helmuthfilho001@gmail.com>
2020-07-19 22:13:45 -0300

    adição de som no bot, adição de bibliotecas e suporte a musica

--- commands/creepy.js
+++ commands/creepy.js
@@ -4,14 +4,25 @@ const ytdl = require('ytdl-core');
 const streamOptions = {seek: 0, volume: 1};
 
 module.exports.run = async (client, message, args) => {
-    //if(message.member.voice.channel){
+    if(message.member.voice.channel){
         message.channel.send('SOCIEDADE MUAHAHAHAHAHA', {
             files: [
                 "./Image/herumutus_creepypasta.png"
             ]
         });
-    //}
-    //else{
-    //    message.channel.send("Amigão, tu precisa estar conectado em um canal de voz... PENSA UM POUCO ANIMAL!!!!!");
-    //}
+
+        message.member.voice.channel.join()
+        .then(connection =>{
+            const stream = ytdl('https://www.youtube.com/watch?v=NwN-hRaQaXM');
+    
+            const DJ = connection.play(stream, streamOptions);
+            DJ.on('finish', () =>{
+                message.member.voice.channel.leave();
+            });
+        })
+        .catch(console.error);
+    }
+    else{
+        message.channel.send("Amigão, tu precisa estar conectado em um canal de voz... PENSA UM POUCO ANIMAL!!!!!");
+    }
 };
