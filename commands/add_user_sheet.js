058cf6a9cdd64ade085f3965ac8d173799b80e35
Helmuth Risch <helmuthfilho001@gmail.com>
2021-03-20 23:32:00 -0300

    termino do comando de adicao de usuario na tabela de autenticacao

--- commands/add_user_sheet.js
+++ commands/add_user_sheet.js
@@ -5,7 +5,12 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
     try
     {
-        console.log(args);
+        if(args.length > 2 || args.length < 2){
+            message.reply("Este comando deve ter até dois argumentos!\n" +
+            "h!add_user_sheet [1] [2]\n" +
+            "Sendo [1]: ID do usuário e [2]: Username do usuário");
+            return;
+        }
 
         if(message.author.id !== process.env.DISCORD_MASTER_USER_ID){
             message.reply("Só o meu criador pode utilizar este comando 🤪");
@@ -23,8 +28,41 @@ module.exports.run = async (client, message, args) => {
 
         let userAuthenticationSheet = doc.sheetsById[process.env.USER_AUTHENTICATION__SHEET_ID];
 
-        await userAuthenticationSheet.addRows({Username: args[0]});
+        let rowsBeforeInsert = await userAuthenticationSheet.getRows();
 
+        let rowsCountBeforeInsert = rowsBeforeInsert.length;
+
+        let insertedRow = await userAuthenticationSheet.addRow({ID: args[0], Username: args[1]});
+
+        //Minus one because the row index considers the header as a row, while .getRows() don't
+        let rowsCountAfterInsert = insertedRow.rowIndex - 1;
+
+        const embed = new Discord.MessageEmbed();
+
+        if(rowsCountAfterInsert > rowsCountBeforeInsert){
+            embed.setColor('#00FF00')
+            .setTitle('Sucesso!')
+            .setURL('')
+            .setAuthor(`${message.author.username}`,`${message.author.displayAvatarURL()}`)
+            .setDescription(`O insert do usuário "${args[0]}" ocorreu com sucesso 😄`)
+            .setThumbnail('https://raw.githubusercontent.com/ashwurz/Herumutus-Bot/master/Image/Bot_Icon_Uptade.png')
+            .setTimestamp(new Date())
+            .setFooter("© Herumutu's BOT Corporation");
+
+            message.channel.send(embed);
+            return;
+        }
+
+        embed.setColor('#FF0000')
+        .setTitle('Erro!')
+        .setURL('')
+        .setAuthor(`${message.author.username}`,'https://www.iconsdb.com/icons/preview/soylent-red/alert-xxl.png')
+        .setDescription(`🤬 Algo de errado aconteceu ao tentar inserir o usuário "${args[0]}". Verifique os logs!!! 🤬`)
+        .setThumbnail('https://raw.githubusercontent.com/ashwurz/Herumutus-Bot/master/Image/sad_man.png')
+        .setTimestamp(new Date())
+        .setFooter("© Herumutu's BOT Corporation");
+
+        message.channel.send(embed);
     }
     catch(err){
         message.reply(`Deu um erro aqui:\n  ${err}`);
